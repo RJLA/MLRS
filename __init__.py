4f2dc6828232ccc2c0bed4454fab3bc6d7420541
Reginald <regi.argamosa@gmail.com>
2021-03-03 14:47:27 +0800

    add init for Raster_to_dataframe

---
+++ __init__.py
@@ -0,0 +1,17 @@
+print(
+'''
+#==============================================================#
+# rs_learn: A library for applying machine learning processes  #
+# and artificial neural network in Remote Sensing              #
+#==============================================================#
+
+#=====================================================================#
+# Author:      Reginald Jay L. Argamosa <regi.argamosa@gmail.com>     #  
+# Affiliation: University of the Philippines -                        #
+#              Training Center for Applied Geodesy and Photogrammetry #
+#=====================================================================#
+'''
+)
+from .Raster_to_dataframe import Raster_to_dataframe
+
+
